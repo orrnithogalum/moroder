18f3e0dc1da074080c339746126fea9afea68595
Leora <syl20.row@gmail.com>
2026-03-08 13:52:54 +0100

    Add comments

--- include/services/mpris.hpp
+++ include/services/mpris.hpp
@@ -1,3 +1,18 @@
+/* MPRIS
+- Provides an interface to sdbus-c++ so that the user can control the player
+  remotely from other applications.
+
+- This implementation is based on code from: https://github.com/chrg127/mpris-server and has been modified for this application's use case.
+- Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files (the "Software"), to deal
+  in the Software without restriction, including without limitation the rights
+  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  copies of the Software, and to permit persons to whom the Software is
+  furnished to do so, subject to the following conditions:
+
+  The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+*/
+
 #include <sdbus-c++/sdbus-c++.h>
 #include <string>
 #include <vector>
